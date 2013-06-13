30f8410f84a4d4ff18bfc98e3444a67bab040eef
Chandler Redding <chandler@dtub.co>
2013-06-12 22:55:22 -0700

    Stuff

--- gameScript.js
+++ gameScript.js
@@ -4,7 +4,10 @@ var context = canvas.getContext('2d');
 // Parameter Variables
 var mPosX;			// Current Mouse Position
 var mPosY;			// Current Mouse Position
+var mClickX;		// Mouse Click Position
+var mClickY; 		// Mouse Click Position
 
+var parts = 0;		// Current screen ID
 var pattern;		// Current fill pattern
 
 var pPosX = 450;	// Initial player position
@@ -18,62 +21,68 @@ var pMoney = 0;		// Player initial Money
 var pAge = 14;		// Player initial Age
 var pMood = true;	// Player initial Mood (true = happy, false = sad)
 
-// Load Image objects here
+// Define Image Objects
 var grass = new Image();
-grass.src = 'img/grass.png';
-var player = new Image();
-player.src = 'img/guy.png';
+var guy = new Image();
 var happy = new Image();
-happy.src = 'img/smile.png';
 var sad = new Image();
-sad.src = 'img/frown.png';
 var pause = new Image();
-pause.src = 'img/pause.png';
 var school = new Image();
-school.src = 'img/school.png';
 var college = new Image();
-college.src = 'img/college.png';
 var university = new Image();
-university.src = 'img/university.png';
 var work = new Image();
+
+// Load Image Objects
+grass.src = 'img/grass.png';
+guy.src = 'img/guy.png';
+happy.src = 'img/smile.png';
+sad.src = 'img/frown.png';
+pause.src = 'img/pause.png';
+school.src = 'img/school.png';
+college.src = 'img/college.png';
+university.src = 'img/university.png';
 work.src = 'img/work.png';
 
 canvas.addEventListener('click', function() {
-	destX = event.offsetX;
-	destY = event.offsetY;
-}, false);
+	mClickX = event.offsetX;
+	mClickY = event.offsetY;
+});
 
 canvas.addEventListener('mousemove', function() {
 	mPosX = event.offsetX;
 	mPosY = event.offsetY;
 });
 
-setInterval(drawMain,40);
+setInterval(draw,40);
 setInterval(age,60000);
 
-function drawMain() {
-	drawScene();
-	movePlayer();
-	drawPlayer();
-	drawBar();
-	drawTip();
+function draw() {
+	switch(parts) {
+		case 1:
+		mainParts();
+		break;
+		default:
+		pauseParts();
+	}
 }
 
-function age() {
-	pAge++;
+function pauseParts() {
+	mainScreen();
+	player();
+	bar();
+	shade();
+	if (250 <= mPosX && mPosX <= 650 && 300 <= mPosY && mPosY <= 345)
+		context.fillStyle = '#bdbdbd';
+	else
+		context.fillStyle = '#539c05';
+	context.fillRect(250, 300, 400, 45);
 }
 
-function drawScene() {
-	pattern = context.createPattern(grass, 'repeat');
-	context.fillStyle = pattern;
-	context.fillRect(0, 0, 900, 500);
-	context.drawImage(school, 20, 55);
-	context.drawImage(college, 20, 175);
-	context.drawImage(university, 20, 295);
-	context.drawImage(work, 750, 100);
-}
+function mainParts() {
+	// Draw Main Scene
+	mainScene();
 
-function movePlayer() {
+	// Move Player
 	if (Math.abs(pPosX - destX) > pSpeed / 25 || Math.abs (pPosY - destY) > pSpeed / 25) {
 		var xDistance = destX - pPosX;
 		var yDistance = destY - pPosY;
@@ -83,28 +92,9 @@ function movePlayer() {
 		pPosX += xMove;
 		pPosY += yMove;
 	}
-}
+	player();
 
-function drawPlayer() {
-	context.drawImage(player, pPosX - 25, pPosY - 25);
-}
-
-function drawBar() {
-	context.fillStyle = '#282828';
-	context.fillRect(0,0,900,35);
-	context.fillStyle = '#ffffff';
-	context.font = '20px Basic Title Font';
-	context.fillText('LifePoints= ' + pLife, 15, 24);
-	context.fillText('Money= @' + pMoney, 315, 24);
-	context.fillText('Age= ' + pAge, 615, 24);
-	if (pMood)
-		context.drawImage(happy, 820, 2);
-	else
-		context.drawImage(sad, 820, 2);
-	context.drawImage(pause, 860, 2);
-}
-
-function drawTip() {
+	// Draw Tips
 	if (20 <= mPosX && mPosX <= 170 && 55 <= mPosY && mPosY <= 155) {
 		context.fillStyle = '#282828';
 		context.fillRect(mPosX, mPosY, 150, 30);
@@ -133,9 +123,49 @@ function drawTip() {
 		context.font = '15px Noto Sans';
 		context.fillText('Go to Work', mPosX -90, mPosY + 20);
 	}
+
+	// Draw Bar
+	bar();
+	pauseButton();
 }
 
-function drawShade() {
+function age() {
+	pAge++;
+}
+
+function mainScreen() {
+	pattern = context.createPattern(grass, 'repeat');
+	context.fillStyle = pattern;
+	context.fillRect(0, 0, 900, 500);
+	context.drawImage(school, 20, 55);
+	context.drawImage(college, 20, 175);
+	context.drawImage(university, 20, 295);
+	context.drawImage(work, 750, 100);
+}
+
+function player() {
+	context.drawImage(guy, pPosX - 25, pPosY - 25);
+}
+
+function bar() {
+	context.fillStyle = '#282828';
+	context.fillRect(0,0,900,35);
+	context.fillStyle = '#ffffff';
+	context.font = '20px Basic Title Font';
+	context.fillText('LifePoints= ' + pLife, 15, 24);
+	context.fillText('Money= @' + pMoney, 315, 24);
+	context.fillText('Age= ' + pAge, 615, 24);
+	if (pMood)
+		context.drawImage(happy, 820, 2);
+	else
+		context.drawImage(sad, 820, 2);
+}
+
+function pauseButton() {
+	context.drawImage(pause, 860, 2);
+}
+
+function shade() {
 	context.globalAlpha = 0.9;
 	context.fillStyle = '#282828';
 	context.fillRect(0, 35, 900, 465);
